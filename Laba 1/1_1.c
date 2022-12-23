b98db9f1805f3f3c14622a19ab57b901d157faf0
DenisAmell <65415967+DenisAmell@users.noreply.github.com>
2022-12-23 23:04:23 +0300

    Update 1_1.c

--- Laba 1/1_1.c
+++ Laba 1/1_1.c
@@ -1,144 +1,213 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
 
+int my_atoi(char s[])
+{
+    int n = 0;
+    for (int i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+    {
 
-void func_h(int num) {
-    int a = 0;
-    for (int i = 0; i < 101; i++) {
-        if (i % num == 0) {
-            printf("%d ", i);
-            a++;
-        }
+        n = 10 * n + (s[i] - '0');
     }
-    if (!a) {
+    return n;
+}
+
+int my_strlen(char s[])
+{
+    int len = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        len++;
+    }
+
+    return len;
+}
+
+void func_h(long int num)
+{
+    int a = 0;
+    // for (int i = 1; i < 101; i++)
+    // {
+    //     if (i % num == 0)
+    //     {
+    //         printf("%d ", i);
+    //         a++;
+    //     }
+    // }
+    int tmp = num;
+    while (tmp <= 100)
+    {
+        printf("%d ", tmp);
+        tmp += num;
+        a++;
+    }
+    if (!a)
+    {
         printf("No such numbers\n");
     }
 }
 
-void func_p(int num) {
-    int d = 0;
-    for (int i = 1; i <= num; i++) {
-        if (num % i == 0) {
-            d++;
+int func_p(long int num)
+{
+    if (num > 1)
+    {
+        if (num == 2)
+        {
+            printf("prime");
+            return 0;
         }
-        if (d > 2) {
-            printf("Input number is not prime");
-            break;
+        if (num & 1 == 0)
+        {
+            printf("Not prime");
+            return 0;
         }
+        for (int i = 2; i <= floor(sqrt(num)); i++)
+        {
+            if (num % i == 0)
+            {
+                printf("Not prime");
+                return 0;
+            }
+        }
+        printf("Prime");
+        return 1;
     }
-    if (d == 2) {
-        printf("Input number is prime");
+    else
+    {
+        printf("Not prime");
+        return 0;
     }
 }
 
-void func_s(int num) {
-    int tmp = num, len = 0;
-    while (tmp) {
-        len++;
-        tmp /= 10;
-    }
-
-    tmp = len;
-
-    int res[len];
-    while (num) {
-        res[len - 1] = num % 10;
-        len--;
-        num /= 10;
-    }
-    for (int i = 0; i < tmp; i++) {
-        printf("%d ", res[i]);
+void func_s(char *num)
+{
+    int len = my_strlen(num);
+    for (int i = 0; i < len; i++)
+    {
+        printf("%c ", num[i]);
     }
 }
 
-void func_e(int num) {
-    if(num>10) {
+void func_e(long int num)
+{
+    if (num > 10)
+    {
         printf("Input number must be <= 10");
-    } else {
-        for (int i = 1; i <= 10; i++) {
+    }
+    else
+    {
+        for (int i = 1; i <= 10; i++)
+        {
             printf("for %d: ", i);
-            unsigned long res = 1;
-            for (int j = 1; j <=num; j++) {
+            long long res = 1;
+            for (int j = 1; j <= num; j++)
+            {
                 res *= i;
-                printf("%lu ", res);
+                printf("%lld ", res);
             }
             printf("\n");
         }
     }
 }
 
-void func_a(int num){
+void func_a(int num)
+{
     int res = 0;
-    for (int i = 1; i <=num; i++) {
-        if(INT_MAX-i<res)
+    for (int i = 1; i <= num; i++)
+    {
+        if (INT_MAX - i < res)
+        {
+            printf("Overflow error!\n");
+            break;
+        }
+        else
+        {
+            res += i;
+        }
+    }
+    if (res)
+    {
+        printf("%d", res);
+    }
+}
+
+void func_f(long int num)
+{
+    unsigned long long res = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        if (ULLONG_MAX / i < res)
         {
             printf("Overflow error!\n");
             res = 0;
             break;
-        } else {
-            res += i;
         }
-    }
-    if (res) {
-        printf("%d", res);
-    }
-
-}
-
-void func_f(int num) {
-    unsigned long long res = 1;
-    for (int i = 2; i <=num; i++) {
-        if (ULLONG_MAX / i < res) {
-            printf("Overflow error!\n");
-            res = 0;
-            break;
-        } else {
+        else
+        {
             res *= i;
         }
     }
-    if (res) {
+    if (res)
+    {
         printf("%d", res);
     }
-
 }
 
-int main(int argc, char* argv[]){
-    if ((argv[2][0] != '-') || (argv[2][0] != '/')) {
+int main(int argc, char *argv[])
+{
+    if (my_strlen(argv[2]) != 2 || argv[2][0] != '-' && argv[2][0] != '/')
+    {
         printf("Input error: wrong flag");
         return 0;
     }
-    if(strlen(argv[2])!=2){
-        printf("Input error: wrong flag!");
-        return 0;
-    }
-    if(argc < 3){
+    if (argc < 3)
+    {
         printf("Input error: few arguments!");
         return 0;
     }
-    else{
-        if(argc == 3 && argv[2][1] == 'c'){
+    else
+    {
+        if (argc == 3 && argv[2][1] == 'c')
+        {
             printf("Input error: too few arguments for that flag!");
             return 0;
         }
-        if(argc>4){
+        if (argc > 3)
+        {
             printf("Input error: too many arguments!");
             return 0;
         }
     }
-    if (argv[2][1] == 'h') {
-        func_h(argv[1]);
-    } else if (argv[2][1] == 'p') {
-        func_p(argv[1]);
-    } else if (argv[2][1] == 's') {
+    if (argv[2][1] == 'h')
+    {
+        func_h(my_atoi(argv[1]));
+    }
+    else if (argv[2][1] == 'p')
+    {
+        func_p(my_atoi(argv[1]));
+    }
+    else if (argv[2][1] == 's')
+    {
         func_s(argv[1]);
-    } else if (argv[2][1] == 'e') {
-        func_e(argv[1]);
-    } else if (argv[2][1] == 'a') {
-        func_a(argv[1]);
-    } else if (argv[2][1] == 'f') {
-        func_f(argv[1]);
-    } else {
+    }
+    else if (argv[2][1] == 'e')
+    {
+        func_e(my_atoi(argv[1]));
+    }
+    else if (argv[2][1] == 'a')
+    {
+        func_a(my_atoi(argv[1]));
+    }
+    else if (argv[2][1] == 'f')
+    {
+        func_f(my_atoi(argv[1]));
+    }
+    else
+    {
         printf("Input error: wrong flag!");
     }
     return 0;
