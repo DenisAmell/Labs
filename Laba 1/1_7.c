e58d90410a2a69f294df3aaef08c07293553741d
DenisAmell <65415967+DenisAmell@users.noreply.github.com>
2022-12-23 23:07:24 +0300

    Update 1_7.c

--- Laba 1/1_7.c
+++ Laba 1/1_7.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-int* create_arr(int* arr, int size, int a, int b) {
-    for (int i = 0; i < size; i++) {
+int *create_arr(int *arr, int size, int a, int b)
+{
+    for (int i = 0; i < size; i++)
+    {
         arr[i] = rand() % (b - a + 1) + a;
     }
     return arr;
 }
 
-void output_arr(int* arr, int size)
+void output_arr(int *arr, int size)
 {
     for (int i = 0; i < size; i++)
     {
@@ -18,21 +20,28 @@ void output_arr(int* arr, int size)
     printf("\n");
 }
 
-void swap(int *minPtr, int *maxPtr) {
+void swap(int *minPtr, int *maxPtr)
+{
     int tmp = *minPtr;
     *minPtr = *maxPtr;
     *maxPtr = tmp;
 }
 
-void find_min_max(int *arr, int size) {
-    int min = arr[0]; int max = arr[0];
+void find_min_max(int *arr, int size)
+{
+    int min = arr[0];
+    int max = arr[0];
     int *max_A = arr;
     int *min_A = arr;
-    for (int i = 0; i < size; i++) {
-        if (arr[i] > max) {
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
             max = arr[i];
             max_A = arr + i;
-        } else if (arr[i] < min) {
+        }
+        else if (arr[i] < min)
+        {
             min = arr[i];
             min_A = arr + i;
         }
@@ -42,18 +51,19 @@ void find_min_max(int *arr, int size) {
     return swap(min_A, max_A);
 }
 
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     srand(time(0));
     int size, a, b;
 
-
     printf("Enter the size of the array: ");
-    if(!scanf("%d", &size)){
+    if (!scanf("%d", &size))
+    {
         printf("Input error: wrong size");
         return 0;
     }
-    if(size<=0){
+    if (size <= 0)
+    {
         printf("Input error: wrong size");
         return 0;
     }
@@ -61,8 +71,13 @@ int main(int argc, char* argv[]) {
     printf("Range of numbers: ");
     scanf("%d%d", &a, &b);
 
-    int* A = (int*)malloc(sizeof(int) * size);
+    int *A = (int *)malloc(sizeof(int) * size);
 
+    if (A == NULL)
+    {
+        printf("Error: not memory");
+        return -1;
+    }
 
     create_arr(A, size, a, b);
     printf("Array: ");
