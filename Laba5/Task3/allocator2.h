a9d94b7f3ba73dde04b73ca2116747022b918ed8
DenisAmell <denisgorschenko04@gmail.com>
2023-04-18 19:42:09 +0300

    list_memory

--- Laba5/Task3/allocator2.h
+++ Laba5/Task3/allocator2.h
@@ -1,8 +1,7 @@
 #ifndef ALLOCATOR2_H
 #define ALLOCATOR2_H
 
-#include "../Task2/allocator.h"
-#include "../Task2/memory_denis.h"
+#include "memory.h"
 #include "../logger/log2/logger.h"
 #include "../logger/log2/logger_builder.h"
 #include "../logger/log2/logger_builder_concrete.h"
@@ -12,7 +11,7 @@
 class My_memory : public memory
 {
 private:
-    void *_ptr;
+    void *_ptr_to_memory;
 
 public:
     enum class mode
@@ -20,6 +19,14 @@ public:
         first_fit,
         best_fit,
         worst_fit
+    };
+    class dealloc_fail final : public std::exception
+    {
+    public:
+        char const *what() const noexcept override
+        {
+            return "Failed to deallocate";
+        }
     };
 
 public:
@@ -31,15 +38,21 @@ public:
 public:
     virtual void *allocate(size_t target_size) const override;
 
-protected:
-    virtual void *get_pointer(void *target_ptr) const;
+private:
+    virtual void *get_pointer_block(void *target_ptr) const;
     virtual size_t get_size_block(void *target_ptr) const;
+    virtual void *const get_first_avail_block() const;
+    virtual void **get_first_avail_block_ptr() const;
+    virtual size_t get_service_part_allocator_size() const;
+    virtual size_t get_service_part_avail_block_size() const;
+    virtual logger *get_logger() const;
+    void dump_allocator_state(bool is_allocate) const noexcept;
+    virtual void *get_right_border_of_memory_ptr() const;
+    virtual std::string get_pointer_address(void *target_ptr) const;
+    virtual void debug_alloc(const void *target_ptr) const;
 
 public:
     virtual void deallocate(void const *const target_to_dealloc) const override;
-
-public:
-    virtual void debug_alloc(const void *target_ptr, size_t size, logger *log) const override;
 
 public:
     My_memory(logger *log, memory *allocator, size_t size, mode method);
--- Laba5/Task3/main.cpp
+++ Laba5/Task3/main.cpp
@@ -4,23 +4,68 @@
 #include "../logger/log2/logger_builder.h"
 #include "../logger/log2/logger_builder_concrete.h"
 #include "../logger/log2/logger_concrete.h"
-#include "../Task2/memory_denis.h"
-#include "../Task2/allocator.h"
+#include "memory.h"
 #include <cstring>
+#include <list>
 int main()
 {
-    logger_builder *builder = new logger_builder_concrete;
-    logger *log = builder->add_stream("file1.txt", logger::severity::debug)->construct();
+    logger_builder *builder = new logger_builder_concrete();
+    logger *log = builder
+                      ->add_stream("file1.txt", logger::severity::trace)
+                      ->add_stream("file2.txt", logger::severity::debug)
+                      //->add_stream("console", logger::severity::trace)
+                      ->construct();
     void *ptr;
-    memory *allocator = new memory_denis(log);
-    memory *allocator2 = new My_memory(log, allocator, 1000, My_memory::mode::first_fit);
+    memory *all2 = new My_memory(log, nullptr, 1000000, My_memory::mode::first_fit);
 
-    int *p = reinterpret_cast<int *>(allocator2->allocate(sizeof(int) * 25));
+    std::list<void *> allocated_blocks;
+    srand((unsigned)time(nullptr));
 
-    for (int i = 0; i < 25; i++)
+    for (int i = 0; i < 1000; i++)
     {
-        p[i] = i;
-        std::cout << p[i] << std::endl;
+        // std::cout << "Iteration #" << i << ":" << std::endl;
+        switch (rand() % 2)
+        {
+        case 0:
+            try
+            {
+                allocated_blocks.push_back(all2->allocate(rand() % 201 + 150));
+            }
+            catch (std::bad_alloc const &ex)
+            {
+                std::cout << i << " allocate failed" << std::endl;
+            }
+            break;
+        case 1:
+            try
+            {
+                if (allocated_blocks.empty())
+                {
+                    // std::cout << i << " nothing to deallocate" << std::endl;
+                    continue;
+                }
+
+                auto ptr_to_deallocate_index = rand() % allocated_blocks.size();
+                auto it = allocated_blocks.begin();
+                std::advance(it, ptr_to_deallocate_index);
+                all2->deallocate(*it);
+                allocated_blocks.erase(it);
+            }
+            catch (My_memory::dealloc_fail const &ex)
+            {
+                std::cout << i << " deallocate failed" << std::endl;
+            }
+            break;
+        }
+    }
+
+    while (!allocated_blocks.empty())
+    {
+        auto ptr_to_deallocate_index = rand() % allocated_blocks.size();
+        auto it = allocated_blocks.begin();
+        std::advance(it, ptr_to_deallocate_index);
+        all2->deallocate(*it);
+        allocated_blocks.erase(it);
     }
 
     // int *p2 = reinterpret_cast<int *>(allocator2->allocate(sizeof(int) * 25));
@@ -32,10 +77,9 @@ int main()
     //     std::cout << p2[i] << std::endl;
     // }
 
-    char *text = reinterpret_cast<char *>(allocator2->allocate(sizeof(char) * 11));
-    strcpy(text, "1234567890");
-    delete allocator2;
-    delete allocator;
+    // char *text = reinterpret_cast<char *>(allocator2->allocate(sizeof(char) * 11));
+    // strcpy(text, "1234567890");
+    delete all2;
     delete log;
     delete builder;
 }
