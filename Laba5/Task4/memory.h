9002df0a1c11800dd60986e5fc9482c32a9eda76
DenisAmell <denisgorschenko04@gmail.com>
2023-04-19 23:16:04 +0300

    border descriptors memory

---
+++ Laba5/Task4/memory.h
@@ -0,0 +1,18 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <iostream>
+
+class memory
+{
+public:
+    virtual void *allocate(size_t target_size) const = 0;
+
+public:
+    virtual void deallocate(void const *const target_to_dealloc) const = 0;
+
+public:
+    virtual ~memory() = default;
+};
+
+#endif // MEMORY_H
