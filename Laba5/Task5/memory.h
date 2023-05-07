fabc1d5e0e1ef5bf54b91acf270507bfc4c0a067
DenisAmell <denisgorschenko04@gmail.com>
2023-05-07 16:09:55 +0300

    task5

--- Laba5/Task5/memory.h
+++ Laba5/Task5/memory.h
@@ -2,9 +2,38 @@
 #define MEMORY_H
 
 #include <iostream>
+#include "../logger/log2/logger.h"
+#include "../logger/log2/logger_concrete.h"
+#include "../logger/log2/logger_holder.h"
 
 class memory
 {
+public:
+    enum class allocate_mode
+    {
+        first_fit,
+        best_fit,
+        worst_fit
+    };
+
+private:
+    virtual size_t get_block_avail_size(void *target_ptr) const = 0;
+    virtual size_t get_occup_block_serv_size() const = 0;
+    virtual size_t get_avail_block_serv_size() const = 0;
+    virtual void *get_pointer_avail_prev_block(void *target_ptr) const = 0;
+    virtual void *get_pointer_avail_next_block(void *target_ptr) const = 0;
+    virtual void *get_first_avail_block() const = 0;
+    virtual void **get_first_avail_block_ptr() const = 0;
+    virtual size_t get_service_part_allocator_size() const = 0;
+    virtual void dump_allocator_state(bool is_allocate) const noexcept = 0;
+    virtual std::string get_pointer_address(void *target_ptr) const = 0;
+    virtual void debug_alloc(const void *target_ptr) const = 0;
+    virtual std::string get_allocate_mode_string(allocate_mode method) const = 0;
+    virtual logger *get_logger() const noexcept = 0;
+    virtual unsigned char compact_block_size_and_block_occup(size_t size_as_power_of_2, bool block_occup) const = 0;
+    virtual void *get_buddies(void *target_ptr) const = 0;
+    virtual bool is_free_block(void *target_ptr) const = 0;
+
 public:
     virtual void *allocate(size_t target_size) const = 0;
 
--- Laba5/Task5/memory_buddy_system.h
+++ Laba5/Task5/memory_buddy_system.h
@@ -2,12 +2,33 @@
 #define MEMORY_BUUDDY_SYSTEM_H
 
 #include "memory.h"
+#include "../logger/log2/logger.h"
+#include "../logger/log2/logger_concrete.h"
+#include "../logger/log2/logger_holder.h"
 
-class memory_buddy_system final : public memory
+class memory_buddy_system final : public memory, private logger_holder
 {
 
 private:
     void *_all_memory;
+
+private:
+    size_t get_block_avail_size(void *target_ptr) const override;
+    size_t get_occup_block_serv_size() const override;
+    size_t get_avail_block_serv_size() const override;
+    void *get_pointer_avail_prev_block(void *target_ptr) const override;
+    void *get_pointer_avail_next_block(void *target_ptr) const override;
+    void *get_first_avail_block() const override;
+    void **get_first_avail_block_ptr() const override;
+    size_t get_service_part_allocator_size() const override;
+    void dump_allocator_state(bool is_allocate) const noexcept override;
+    std::string get_pointer_address(void *target_ptr) const override;
+    void debug_alloc(const void *target_ptr) const override;
+    std::string get_allocate_mode_string(allocate_mode method) const override;
+    logger *get_logger() const noexcept override;
+    unsigned char compact_block_size_and_block_occup(size_t size_as_power_of_2, bool block_occup) const override;
+    void *get_buddies(void *target_ptr) const override;
+    bool is_free_block(void *target_ptr) const override;
 
 public:
     virtual void *allocate(size_t target_size) const override;
@@ -16,7 +37,8 @@ public:
     virtual void deallocate(void const *const target_to_dealloc) const override;
 
 public:
-    virtual ~memory_buddy_system();
+    memory_buddy_system(logger *log, memory *allocator, size_t size_as_power_of_2, allocate_mode method);
+    ~memory_buddy_system();
 };
 
-#include // MEMORY_BUUDDY_SYSTEM_H
+#endif // MEMORY_BUUDDY_SYSTEM_H
